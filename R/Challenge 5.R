e50b07cd2b5d506e1b4b67b026a888b8fa6a3389
amashford <46579863+amashford@users.noreply.github.com>
2019-07-18 10:38:30 +0100

    Tidy code and move to R folder

--- R/Challenge 5.R
+++ R/Challenge 5.R
@@ -1,33 +1,18 @@
-#Challenge 5
-#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-
-#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-
-
+# Challenge 5
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 #Check Function
 divide_function <- function(i) {
-  
   for(j in 11:20){
-    
-    if(i%%j == 0){
-      
+    if(i %% j == 0){
       result <- TRUE
-      
     } else {
-      
       result <- FALSE
-      
       break
-      
     }
-    
-  }
-  
   print(result)
-  
 }
-
 
 #Setup
 step <- 2560
@@ -36,24 +21,15 @@ i <- 2560
 
 solution <-  FALSE
 
-
 #Loop
 while(solution == FALSE){
-  
   if(divide_function(i) == TRUE) {
-    
     solution <-  TRUE
-    
     break
-    
     print(i)
-    
   } else {
-    
     i <- i + step
-    
   }
-  
 }
 
-#232792560
+#Answer is 232,792,560
